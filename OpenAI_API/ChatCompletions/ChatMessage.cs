0d7636b9091c5c8a31a9b1ef035ac909260bc0b5
Nicolas Vandeginste <nicolas.vandeginste@signature.one>
2023-03-08 00:28:21 +0100

    Added name property.

--- OpenAI_API/ChatCompletions/ChatMessage.cs
+++ OpenAI_API/ChatCompletions/ChatMessage.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace OpenAI_API.ChatCompletions
 {
@@ -6,6 +6,8 @@ namespace OpenAI_API.ChatCompletions
     {
         [JsonProperty("role")]
         public string Role { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
         [JsonProperty("content")]
         public string Content { get; set; }
 
